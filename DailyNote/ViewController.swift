c97d9fb5c1309aa59e3392b8e618ce24e9c31c27
89Explorer <leral3@naver.com>
2024-11-20 15:31:19 +0900

    Initial Commit

---
+++ DailyNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyNote
+//
+//  Created by 권정근 on 11/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DailyNoteTests/DailyNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNoteTests.swift
+//  DailyNoteTests
+//
+//  Created by 권정근 on 11/20/24.
+//
+
+import Testing
+@testable import DailyNote
+
+struct DailyNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
